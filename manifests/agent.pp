30b14a0352646b0a4a2fc4ee55e09a6f710f713b
Alex Crowe <alex@pancentric.com>
2013-08-31 21:48:19 +0100

    added first readme and fixed variable calls

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -5,9 +5,9 @@ class zabbix::agent (
     $logfile      = '/var/log/zabbix-agent/zabbix_agentd.log',
     $num_agents   = '5',
     $debug_level  = '3',
-    $agent_listen = false,
-    $agent_listen_port = '10050',
-    $agent_listen_host = 'localhost',
+    $agent_listen = true,
+    $listen_port = '10050',
+    $listen_host = '0.0.0.0',
     $remote_cmd   = false,
   ) {
 
